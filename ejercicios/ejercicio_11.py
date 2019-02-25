f376cc2dfbbb32233df699528c1698b13f606247
Jorge Mauricio <jorge.ernesto.mauricio@gmail.com>
2019-02-25 10:15:56 -0600

    agregar ejercicio 11 y 12

---
+++ ejercicios/ejercicio_11.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#  Author: Jorge Mauricio
+#  Email: jorge.ernesto.mauricio@gmail.com
+#  Date: 2018-02-01
+#  Version: 1.0
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+Objetivo:
+Escribe un programa que toma una frase ingresada por el usuario y contabilice el numero
+de letras y números que contiene la frase.
+
+Ej
+	Hola mundo! 1234
+
+Resultado:
+    Números = 4
+    Letras = 9
+
+"""
---
+++ ejercicios/ejercicio_12.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#  Author: Jorge Mauricio
+#  Email: jorge.ernesto.mauricio@gmail.com
+#  Date: 2018-02-01
+#  Version: 1.0
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+Objetivo:
+Define una función en la cual se acepten dos variables string, evalue cual
+de las dos funciones tiene el número mayor de caracteres y la imprima, si
+las dos palabras tienen un número igual de caracteres, se deben de imprimir
+las dos palabras
+
+Ej.
+    hola
+    adios
+
+Resultado:
+    adios
+
+"""
