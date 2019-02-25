a841723277dd94c1bad411bb518885029f67e408
Jorge Mauricio <jorge.ernesto.mauricio@gmail.com>
2019-02-25 09:56:38 -0600

    agregar ejericico 10

---
+++ ejercicios/ejercicio_10.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#  Author: Jorge Mauricio
+#  Email: jorge.ernesto.mauricio@gmail.com
+#  Date: 2018-02-01
+#  Version: 1.0
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+De la siguiente matrix de datos, imprime el valor y la posición
+de los 5 numéros con el valor más alto
+matrix = [[20,1,20,8,6],[19,0,5,8,10],[9,8,9,0,12],[11,2,13,4,5],[4,3,18,1,3]]
+
+Resultado:
+Valor 20, posición 0,0
+Valor 20, posición 0,2
+Valor 20, posición 3,2
+Valor 19, posición 1,0
+Valor 18, posición 4,2
+"""
