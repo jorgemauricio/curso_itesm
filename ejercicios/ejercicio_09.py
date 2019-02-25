de9220782cd9b876dfc6ec3b45b12096e3b561de
Jorge Mauricio <jorge.ernesto.mauricio@gmail.com>
2019-02-25 09:54:34 -0600

    agregar ejercicio 9

---
+++ ejercicios/ejercicio_09.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#  Author: Jorge Mauricio
+#  Email: jorge.ernesto.mauricio@gmail.com
+#  Date: 2018-02-01
+#  Version: 1.0
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+Objetivo:
+De una matrix dada, encontrar el máximo producto que resulte de la multiplicación
+de cinco de sus elementos
+
+matrix = [[20,1,20,8,6],[19,0,5,8,10],[9,8,9,0,12],[11,2,13,4,5],[4,3,18,1,3]]
+
+Resultado: 1778400
+
+"""
